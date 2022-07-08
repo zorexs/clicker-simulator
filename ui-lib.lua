d28cfa6a29a568114cb56e77b5798f2cb6f8fe32
zorex <64035181+zorexs@users.noreply.github.com>
2022-07-08 19:05:46 +0300

    Update ui-lib.lua

--- ui-lib.lua
+++ ui-lib.lua
@@ -210,7 +210,7 @@ function finity.new(isdark, gprojectName, thinProject)
 		BackgroundColor3 = theme.main_container,
 		BorderSizePixel = 0,
 		Position = UDim2.new(0.5, 0, 0.5, 0),
-		Size = UDim2.new(0, 500, 0, 500),
+		Size = UDim2.new(0, 600, 0, 500),
 		ZIndex = 2,
 		ImageTransparency = 1
     })
